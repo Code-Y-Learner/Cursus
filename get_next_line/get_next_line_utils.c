419463996f3e75b3fc39119c530f644dfc02d67c
Code-Y-Learner <root@DESKTOP-99BHMHK.localdomain>
2023-04-14 13:10:47 +0900

    utils

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,61 +6,90 @@
 /*   By: seungjyu <seungjyu@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:38:00 by seungjyu          #+#    #+#             */
-/*   Updated: 2023/04/06 21:18:42 by seungjyu         ###   ########.fr       */
+/*   Updated: 2023/04/09 00:24:45 by seungjyu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
+#include "get_next_line.h"
 #include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
-
-#define BUFFER_SIZE 1
 
 size_t	ft_strlen(const char *str)
 {
 	int	i;
 
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 	return (i);
 }
 
-char	*ft_strdup(char *src)
+char	*ft_strchr(char *s, int c)
 {
 	int		i;
-	char	*dest;
 
 	i = 0;
-	while (src[i])
-		i++;
-	dest = (char *)malloc(500);
-	if (!dest)
+	if (!s)
 		return (0);
-	i = 0;
-	while (src[i])
+	if (c == '\0')
+		return (&((char *)s)[ft_strlen(s)]);
+	while (s[i])
 	{
-		dest[i] = src[i];
+		if (s[i] == (char)c)
+			return (&((char *)s)[i]);
 		i++;
 	}
-	dest[i] = '\0';
+	return (0);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	int	i;
+
+	if (src == 0 && dest == 0)
+		return (0);
+	if (src >= dest)
+	{
+		i = 0;
+		while (i < n)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+		return (dest);
+	}
+	i = (int)n;
+	while (--i >= 0)
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 	return (dest);
 }
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strjoin_expand(char *s1, char const *s2)
 {
-	if (!src)
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*str;
+	size_t	i;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = (char *)malloc(len_s1 + len_s2 + 1);
+	if (!str)
 		return (0);
-	while (*dest)
-		dest++;
-	while (*src)
+	i = 0;
+	while (i < len_s1)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		str[i] = s1[i];
+		i++;
 	}
-	*dest = '\0';
-	return (dest);
+	while (i < len_s1 + len_s2)
+	{
+		str[i] = s2[i - len_s1];
+		i++;
+	}
+	str[i] = '\0';
+	if (s1)
+		free(s1);
+	return (str);
 }
