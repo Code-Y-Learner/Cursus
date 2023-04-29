f003421b63157c9a31cf52194c2e7001b87918b1
Seungjae Yu <seungjyu@c1r9s7.42seoul.kr>
2023-04-29 13:52:07 +0900

    update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,3 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdarg.h>
+#include <stdio.h>
+
+int ft_printf(const char *format, ...);
+int	check_format(const char *str, va_list ap);
+
+int	check_format(const char *str, va_list ap)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == 's')
+		printf("%s", va_arg(ap, char *));
+	elif (str[i] == '')
+	return (0);
+}
+
+int ft_printf(const char *format, ...)
+{
+	int		i;
+	va_list	ap;
+
+	i = 0;
+	va_start(ap, format);
+	while(format[i])
+	{
+		if (format[i] == '%')
+			check_format(&format[++i], ap);
+		i++;
+	}
+	return (-1);
+}
+
+
+int	main(void)
+{
+	ft_printf("%s", "this is great");
+}
+
